7dd12056973de9ca9e5211232c4845daf92ceefb
Spafi <63115231+Spafi@users.noreply.github.com>
2023-06-29 17:15:29 +0300

    Description improvements

--- basics.js
+++ basics.js
@@ -26,7 +26,7 @@
     anotherVariable = 'Hello, I changed';
 
     /*
-	But if we try to change to the 'const' variable
+	But if we try to change the value of the 'const' variable
 	We would get an error
 	*/
     // iCantChange = 'something else'  // We would get an error here
@@ -44,18 +44,18 @@
 {
     // DATA TYPES
 
-    let aString = 'Salam'; // aString is a string
+    let aString = 'Salam';                // aString is a string
 
-    let aNumber = 25; // age is a number
+    let aNumber = 25;                     // age is a number
 
-    let likesBananas = true; // likeBananas is a boolean
+    let likesBananas = true;              // likeBananas is a boolean
 
-    let hobbies = ['hiking', 'running']; // hobbies is a list of strings
+    let hobbies = ['hiking', 'running'];  // hobbies is a list of strings
 
     let person = {
         name: 'John',
         age: 24,
-    }; // person is an object with 2 properties
+    };                                    // person is an object with 2 properties
 
     // If we want to get the name property from the person, we can acces it with a '.'
     let personName = person.name; // personName = 'John'
@@ -79,7 +79,7 @@
     let temperature = 20;
 
     if (temperature > 15) {
-        // if temperature is > 10 ONLY this part will be executed
+        // if temperature is > 15 ONLY this part will be executed
         console.log('no jacket needed');
     } else {
         // else, ONLY this part will be executed
@@ -130,17 +130,18 @@
     let variableAtTop = 'i can appear down the line';
 
     {
-        console.log(variableAtTop); // i can appear down the line';
+        console.log(variableAtTop);               // i can appear down the line';
 
         let variableDeep = 'i am inside many code blocks';
-        console.log(variableDeep); //i am inside many code blocks';
+                     
         // we can use variableDeep here
+	console.log(variableDeep);               //i am inside many code blocks';
     }
     // we can use variableAtTop here
-    console.log(variableAtTop); // i can appear down the line';
+    console.log(variableAtTop);                  // i can appear down the line';
 
     // we CAN'T use variableDeep here
-    // console.log(variableDeep); // ERROR
+    // console.log(variableDeep);                // ERROR
 }
 // --------------------------------------------------------------
 
@@ -149,25 +150,25 @@
 
     // Each element of an array has an index, starting with 0
     let users = [
-        'Daniel', // index 0
-        'Earl', // index 1
-        'Virgie', // index 2
-        'Augusta', // index 3
-        'Elva', // index 4
-        'Julia', // index 5
-        'Cole', // index 6
+        'Daniel',    // index 0
+        'Earl',      // index 1
+        'Virgie',    // index 2
+        'Augusta',   // index 3
+        'Elva',      // index 4
+        'Julia',     // index 5
+        'Cole',      // index 6
     ];
 
     // If we want to get an element at a specific index we can do
-    const firstElement = users[0]; // Daniel
-    const secondElement = users[1]; // Earl
-    const lastElement = users[6]; // Cole
+    const firstElement = users[0];   // Daniel
+    const secondElement = users[1];  // Earl
+    const lastElement = users[6];    // Cole
 
     // Each array has a property named length that stores the length of the array (how many elements are in it)
     // If we want to see how many elements we have in the array we can do
-    var usersCount = users.length; // 7, because we have 7 users in the array
+    let usersCount = users.length;     // usersCount =  7, because we have 7 users in the array
 
-    // Each array has also some pre made functions for manipulating the data (adding, removing, updating)
+    // Each array has also some premade functions for manipulating the data (adding, removing, updating, etc...)
 
     // If we want to add another user in the array we can use the method 'push()  -> array.push(element)'
 
@@ -176,7 +177,21 @@
 
     // Call the 'push()' method on our array
     users.push(newUser); // Now, our users array has one more user, 'Salam'
-
+    /*    
+    let users = [
+        'Daniel',    // index 0
+        'Earl',      // index 1
+        'Virgie',    // index 2
+        'Augusta',   // index 3
+        'Elva',      // index 4
+        'Julia',     // index 5
+        'Cole',      // index 6
+	'Salam',     // index 7    <-- our new user
+    ];
+    */
+    // If we check the length property now, it will be 8 because we added a new user
+    console.log(users.length)  // 8
+ 
     // If we want to remove the last user in the array we can use the method 'pop()  -> array.pop()'
 
     // Call the 'pop()' method on our array
@@ -189,18 +204,18 @@
 {
     // LOOPS
     let users = [
-        'Daniel', // index 0
-        'Earl', // index 1
-        'Virgie', // index 2
-        'Augusta', // index 3
-        'Elva', // index 4
-        'Julia', // index 5
-        'Cole', // index 6
+        'Daniel',    // index 0
+        'Earl',      // index 1
+        'Virgie',    // index 2
+        'Augusta',   // index 3
+        'Elva',      // index 4
+        'Julia',     // index 5
+        'Cole',      // index 6
     ];
 
     // We have multiple methods to iterate over an array
 
-    // A simple for loop
+    // A simple for loop that uses 
     for (let index = 0; index < users.length; index++) {
         console.log(users[index]);
     }
@@ -241,22 +256,21 @@
     const newWay = `${userName} is ${userAge} years old! His favorite animal is ${favoriteAnimal}, and he likes to eat ${favoriteFood}`;
 
     // Both oldWay and newWay will display
-    // Ion is 23 years old! His favorite animal is cat, and he likes to eat pizza!
+    // Jonathan is 23 years old! His favorite animal is cat, and he likes to eat pizza!
 }
 // --------------------------------------------------------------
 
 {
     // FUNCTIONS
 
-    // To declare a function, we can use
-    // The declaration is like a blueprint that says what will happen when we call the function later
+    // The declaration of a function is like a blueprint that says what will happen when we call the function later
 
     function aFunction() {
         // Do something
         console.log('In a function');
     }
 
-    // When we call a function, it will go to the declaration and figure out what to do
+    // When we call a function, it will go to the declaration and figure out what to do, in our case it logs to the console "In a function" 
     aFunction();
 
     // A function that displays 'Hello, user!' for a passed user
@@ -273,27 +287,31 @@
     // We call the greetUser function for user
     greetUser(user);
 
+
+
+    // A nested function
+
     // We can declare another function that uses our previous function
 
     function greetUsersFromList(usersList) {
         for (let user of usersList) {
-            greetUser(user);
+            greetUser(user);  // our previous function
         }
     }
 
     // And call it for a list of users
 
     let users = [
-        'Daniel', // index 0
-        'Earl', // index 1
-        'Virgie', // index 2
-        'Augusta', // index 3
-        'Elva', // index 4
-        'Julia', // index 5
-        'Cole', // index 6
+        'Daniel',    // index 0
+        'Earl',      // index 1
+        'Virgie',    // index 2
+        'Augusta',   // index 3
+        'Elva',      // index 4
+        'Julia',     // index 5
+        'Cole',      // index 6
     ];
 
-    greetUsersFromList(users);
+    greetUsersFromList(users);  // will log to the console "Hello, user!" for each user in the list ("Hello, Daniel!", "Hello Earl!", ...)
 }
 // --------------------------------------------------------------
 
@@ -308,6 +326,6 @@
     const anArrowFunction = (parameter1, parameter2) => parameter1 + parameter2;
 
     // If we call the functions, they will do the same thing
-    console.log(aClassicFunction(3, 5)); // 8
-    console.log(anArrowFunction(3, 5)); // 8
+    console.log(aClassicFunction(3, 5));  // 8
+    console.log(anArrowFunction(3, 5));   // 8
 }
